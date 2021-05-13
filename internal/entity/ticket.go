041e987001924600b33b214c4210ef6e1c854625
muktiarafi <muktiarafi@gmail.com>
2021-05-13 21:21:17 +0700

    ticket json tag

--- internal/entity/ticket.go
+++ internal/entity/ticket.go
@@ -1,10 +1,10 @@
 package entity
 
 type Ticket struct {
-	ID      int64
-	Title   string
-	Price   float64
-	OrderID int64
-	UserID  int64
-	Version int64
+	ID      int64   `json:"id"`
+	Title   string  `json:"title"`
+	Price   float64 `json:"price"`
+	OrderID int64   `json:"orderId"`
+	UserID  int64   `json:"userId"`
+	Version int64   `json:"version"`
 }
